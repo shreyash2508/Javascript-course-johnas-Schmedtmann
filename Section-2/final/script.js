ab7f52bbba940669cfc3db36a9b96be3ddc37c2a
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-07-05 23:13:26 +0530

    Third commit

--- Section-2/final/script.js
+++ Section-2/final/script.js
@@ -162,12 +162,12 @@ if (aged >= 18) {
 // console.log(Boolean({}));
 // console.log(Boolean(''));
 
-// const money = 100;
-// if (money) {
-//   console.log("Don't spend it all ;)");
-// } else {
-//   console.log('You should get a job!');
-// }
+const money = 100;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log('You should get a job!');
+}
 
 // let height = 0;
 // if (height) {
