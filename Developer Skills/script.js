9b3895285e51592d0a371da6222dbf24aaf78062
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-07-06 14:34:22 +0530

    Initial commit

--- Developer Skills/script.js
+++ Developer Skills/script.js
@@ -1,6 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x= 23;
+if (x===23) console.log(23);
+
+const calcage = birthyear => 2037- birthyear;
+
+
 /*
 ///////////////////////////////////////
 // Using Google, StackOverflow and MDN
