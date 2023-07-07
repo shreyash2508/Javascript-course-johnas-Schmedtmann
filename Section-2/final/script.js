d0e2ca0dd30a11dc77d637e1601716e14e5ca186
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-07-07 12:00:22 +0530

    5th commit

--- Section-2/final/script.js
+++ Section-2/final/script.js
@@ -95,10 +95,10 @@ console.log(ageJonas, ageSarah);
 // const ageJonas= now - 2031;
 // const ageSarah= now - 2011;
 
-// console.log(now-2031 > now-2011);
-// let x, y;
-// x = y = 25 - 10 - 5; // x = y = 10, x = 10
-// console.log(x, y);
+console.log(now-2031 > now-2011);
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
 
 // const averageAge = (ageJonas + ageSarah) / 2;
 // console.log(ageJonas, ageSarah, averageAge);
