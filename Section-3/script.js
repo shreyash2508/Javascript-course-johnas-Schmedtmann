ca4a29030878877174b50e40983a03759605f6b2
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-07-08 18:33:33 +0530

    Initial commit

--- Section-3/script.js
+++ Section-3/script.js
@@ -7,3 +7,38 @@ const passTest = true;
 
 if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive :D');
+
+// Functions
+function logger() {
+    console.log('My name is Jonas');
+
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitprocessor(apple,oranges){
+    console.log(apple,oranges);
+    const juice= `Juice with ${apple} apples and ${oranges} oranges.`;
+    console.log(juice);
+}
+
+fruitprocessor(8,0);
+
+// Function declaration
+function calcAge1(birthYeah) {
+    return 2037 - birthYeah;
+  }
+  const age1 = calcAge1(1991);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYeah) {
+    return 2037 - birthYeah;
+  }
+  const age2 = calcAge2(1991);
+  
+  console.log(age1, age2);
+  
