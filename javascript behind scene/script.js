fbcc6cf0a41ea48ec2fd265be87888f664dd95e2
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-07-31 20:24:46 +0530

    Initial Commit

--- javascript behind scene/script.js
+++ javascript behind scene/script.js
@@ -3,38 +3,135 @@
 ///////////////////////////////////////
 // Scoping in Practice
 
-function calcAge(birthyear){
-    const age= 2037 - birthyear;
+// function calcAge(birthyear){
+//     const age= 2037 - birthyear;
     
-    function printAge(){
-        const output= `you are ${age} born in ${birthyear}`
-        console.log(output);
-    }
-    printAge();
-    if (birthyear>=1991 && birthyear <=1996){
-        var millenial = true;
-      // Creating NEW variable with same name as outer scope's variable
-      const firstName = 'Steven';
+//     function printAge(){
+//         const output= `you are ${age} born in ${birthyear}`
+//         console.log(output);
+//     }
+//     printAge();
+//     if (birthyear>=1991 && birthyear <=1996){
+//         var millenial = true;
+//       // Creating NEW variable with same name as outer scope's variable
+//       const firstName = 'Steven';
 
-      // Reasssigning outer scope's variable
-      output = 'NEW OUTPUT!';
+//       // Reasssigning outer scope's variable
+//       output = 'NEW OUTPUT!';
 
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-      console.log(add(2,3));
-    }
+//       function add(a, b) {
+//         return a + b;
+//       }
+//       console.log(add(2,3));
+//     }
 
 
-    console.log(add(2,3));
-    console.log(millenial);
-    // console.log(add(2, 3));
-    console.log(output);
-    return age;
-}
+//     console.log(add(2,3));
+//     console.log(millenial);
+//     // console.log(add(2, 3));
+//     console.log(output);
+//     return age;
+// }
 
-const FirstName= "Jonas";
-calcAge(1991);
+// const FirstName= "Jonas";
+// calcAge(1991);
+
+// ///////////////////////////////////////
+// // Hoisting and TDZ in Practice
+
+// // Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
+
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
+
+// // Functions
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow);
+// // console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// // Example
+// console.log(undefined);
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted!');
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+
+///////////////////////////////////////
+// The this Keyword in Practice
+console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAge(1991);
+
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAgeArrow(1980);
+
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
+
+///////////////////////////////////////
+// Regular Functions vs. Arrow Functions
+
+const Jonas = {
+  firstname: 'Jonas',
+  year: 1991,
+  calcAge: function(){
+    console.log(this);
+    console.log(2037- this.year);
+  },
+
+  greet:() => console.log(`Hey ${this.firstname}`),
+};
+Jonas.greet();
