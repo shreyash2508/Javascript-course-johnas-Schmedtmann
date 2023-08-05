73c248d6b4fd860bf5cfd038cd1fd31992a2aa13
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-08-05 17:00:12 +0530

    Initial Commit

--- modern operators, DS/script.js
+++ modern operators, DS/script.js
@@ -67,30 +67,129 @@ const restaurant = {
 ///////////////////////////////////////
 // Destructuring Objects
 
-const {name, openingHours, categories} = restaurant;
-console.log(name,openingHours,categories);
+// const {name, openingHours, categories} = restaurant;
+// console.log(name,openingHours,categories);
 
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantName, hours, tags);
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantName, hours, tags);
 
-//default values
-const {menu=[], starterMenu: starters=[], }=restaurant;
-console.log(menu,starters);
+// //default values
+// const {menu=[], starterMenu: starters=[], }=restaurant;
+// console.log(menu,starters);
 
-//Mutating Variables
-let a=111;
-let b=999;
+// //Mutating Variables
+// let a=111;
+// let b=999;
 
-const obj= {a:20,b:22,c:14};
-({a,b}=obj);
-console.log(obj);
+// const obj= {a:20,b:22,c:14};
+// ({a,b}=obj);
+// console.log(obj);
 
-// Nested objects
-const {
-  fri: { open: o, close: c },
-} = openingHours;
-console.log(o, c);
+// // Nested objects
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
+
+// ///////////////////////////////////////
+// // The Spread Operator (...)
+
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+
+// const newArr=[1,2,...arr];
+// console.log(newArr);
+
+// console.log(...newArr);
+// console.log(1, 2, 7, 8, 9);
+
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// // Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+
+// // Join 2 arrays
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+// // Iterables: arrays, strings, maps, sets. NOT objects
+
+// const str= 'Jonas';
+// const letters=[...str,'','S '];
+// console.log(...str);
+// console.log(letters);
+
+// ///////////////////////////////////////
+// // Rest Pattern and Parameters
+// // 1) Destructuring
+
+// // SPREAD, because on RIGHT side of =
+// const arr = [1, 2, ...[3, 4]];
+
+// // REST, because on LEFT side of =
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza,risotto,otherFood);
+
+// // Objects
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
+
+// // 2) Functions
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+//   console.log(sum);
+// };
+
+// add(2, 3);
+// add(5, 3, 7, 2);
+// add(8, 2, 5, 3, 2, 1, 4);
+
+// const x= [23,5,7];
+// add(...x);
+
+///////////////////////////////////////
+// Short Circuiting (&& and ||)
+const str= 'Jonas';
+
+console.log(3 || str);
+console.log('' || str);
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'jonas');
+
+///////////////////////////////////////
+// The Nullish Coalescing Operator
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
