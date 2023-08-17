1cd78924117aa193018ddd57731fd2128e5fe34c
shreyashupadhyay.10@gmail.com <shreyashupadhyay.10@gmail.com>
2023-08-17 23:12:36 +0530

    Initial Commit

--- Intl Timers and Dates/Script.js
+++ Intl Timers and Dates/Script.js
@@ -98,3 +98,117 @@
 // console.log(isEven(8));
 // console.log(isEven(23));
 // console.log(isEven(514));
+
+///////////////////////////////////////
+// // Numeric Separators
+
+// // 287,460,000,000
+// const diameter = 287_460_000_000;
+// console.log(diameter);
+
+// const price = 345_99;
+// console.log(price);
+
+// const transferFee1 = 15_00;
+// const transferFee2 = 1_500;
+
+// const PI = 3.1415;
+// console.log(PI);
+
+// console.log(Number('230_000'));
+// console.log(parseInt('230_000'));
+
+// // Working With BigInt
+
+// console.log(2 ** 53 - 1);
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(2 ** 53 + 1);
+// console.log(2 ** 53 + 2);
+// console.log(2 ** 53 + 3);
+// console.log(2 ** 53 + 4);
+
+// // Operations
+// console.log(10000n + 10000n);
+// console.log(36286372637263726376237263726372632n * 10000000n);
+// // console.log(Math.sqrt(16n));
+
+// const huge = 20289830237283728378237n;
+// const num = 23;
+// console.log(huge * BigInt(num));
+
+// // Exceptions
+// console.log(20n > 15);
+// console.log(20n === 20);
+// console.log(typeof 20n);
+// console.log(20n == '20');
+
+// console.log(huge + ' is REALLY big!!!');
+
+// // Divisions
+// console.log(11n / 3n);
+// console.log(10 / 3);
+
+
+// ///////////////////////////////////////
+// // Creating Dates
+
+// // Create a date
+
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date('Aug 02 2020 18:05:41'));
+// console.log(new Date('December 24, 2015'));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+// console.log(new Date(2037, 10, 31));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+
+// // Working with dates
+ const future = new Date(2037, 10, 19, 15, 23);
+// console.log(future);
+// console.log(future.getFullYear());
+// console.log(future.getMonth());
+// console.log(future.getDate());
+// console.log(future.getDay());
+// console.log(future.getHours());
+// console.log(future.getMinutes());
+// console.log(future.getSeconds());
+// console.log(future.toISOString());
+// console.log(future.getTime());
+
+// console.log(new Date(2142256980000));
+
+// console.log(Date.now());
+
+// future.setFullYear(2040);
+// console.log(future);
+
+
+///////////////////////////////////////
+// // Operations With Dates
+// console.log(+future);
+
+// const calcDaysPassed = (date1, date2) =>
+//   Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+// const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+// console.log(days1);
+
+const options = {
+    style: 'currency',
+    unit: 'celsius',
+    currency: 'EUR',
+    // useGrouping: false,
+  };
+  
+  console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num));
+  console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num));
+  console.log('Syria:   ', new Intl.NumberFormat('ar-SY', options).format(num));
+  console.log(
+    navigator.language,
+    new Intl.NumberFormat(navigator.language, options).format(num)
+  );
